f653b94e4c783bc923fb137b89e1b0b977e10dca
Mun Young Cho <munyoungc31@gmail.com>
2024-11-20 18:34:56 -0800

    add playerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = .01f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(moveSpeed * horizontal * Time.deltaTime, moveSpeed * vertical * Time.deltaTime, 0);
+    }
+
+    private void Jump()
+    {
+        // �����̽��ٸ� ������ ���� ����
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); 
+        }
+    }
+    private bool IsGrounded()
+    {
+        // ���� ��Ҵ��� Ȯ�� (�浹 �˻�)
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        float distance = 0.1f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        return hit.collider != null; // ���� �浹 ���̸� true
+    }
+
+}
