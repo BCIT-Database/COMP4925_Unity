42477dd413155d53f1994ce9da14e77ffcdf877b
Yongeun Kwon <70299766+jenkwon92@users.noreply.github.com>
2024-12-01 06:48:01 -0800

    path added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
+const path = require("path");
 require("dotenv").config();
 
 const { database } = require("./databaseConnection");
